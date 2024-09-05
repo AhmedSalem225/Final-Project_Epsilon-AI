14084204f19122dd1063953c93cc9dc116579908
AhmedSalem225 <149170040+AhmedSalem225@users.noreply.github.com>
2024-09-05 17:12:47 +0300

    Add files via upload

---
+++ models/dummies.py
@@ -0,0 +1,63 @@
+Amenity_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Bump_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Crossing_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Give_Way_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Junction_dummies = {
+    'True': [1],
+    'False': [0]
+}
+No_Exit_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Railway_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Roundabout_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Station_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Stop_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Traffic_Calming_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Traffic_Signal_dummies = {
+    'True': [1],
+    'False': [0]
+}
+Traffic__Loop_dummies = {
+    'True': [1],
+    'False': [0]
+}
+
+
+
+
+  
+ 
+
+   
+ 
+  
+ 
